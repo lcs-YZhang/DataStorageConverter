01ca0901746fc803ccdf74a836309f0d2edd03de
Steven Zhang <YZhang@lcs.on.ca>
2021-02-12 11:10:37 -0500

    Added iOS target using SwiftUI framework to project

---
+++ iOS-DataStorageConverter/iOS_DataStorageConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  iOS_DataStorageConverterApp.swift
+//  iOS-DataStorageConverter
+//
+//  Created by Steven Zhang on 2021-02-12.
+//
+
+import SwiftUI
+
+@main
+struct iOS_DataStorageConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
